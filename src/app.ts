35207b3c81e29835d696df8393809eac34834b6f
Hassan <30626086+hassanrobleh@users.noreply.github.com>
2022-03-20 15:07:42 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -18,33 +18,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(routes)
 
-// Routes
-// app.get('/', (req: Request, res: Response, next: NextFunction) => {
-//     res.send("Hello from ts app")
-// })
 
-// app.post('/api/book', (req, res) => {
-//     console.log(req.body)
-
-//     return res.send('')
-// })
-
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//     next(new createHttpError.NotFound())
-// })
-// const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-//     res.status(err.status || 500)
-//     res.send({
-//         status: err.status || 500,
-//         message: err.message
-//     })
-// }
-// app.use(errorHandler)
-
-// db.sync().then(() => {
-//     console.log('connect to db')
-// })
-
+// server
 const server: Server = app.listen(PORT, () => {
     console.log(`Server: Server is running at http://localhost:${PORT}`);
 })
